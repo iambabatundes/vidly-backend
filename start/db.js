30fee6b6232ef1eaa8f78ed41d28511ecdf70f67
Emmanuel Babatunde <iambabatundes@gmail.com>
2023-02-08 06:01:20 -0800

    Adjusting the database

--- start/db.js
+++ start/db.js
@@ -5,9 +5,7 @@ const config = require("config");
 module.exports = function () {
   mongoose.set("strictQuery", true);
 
-  mongoose;
-  const db = config
-    .get("db")
-    .connect(db)
-    .then(() => winston.info(`Connected to ${db}...`));
+  const db = config.get("db");
+
+  mongoose.connect(db).then(() => winston.info(`Connected to ${db}...`));
 };
