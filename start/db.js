18f32f77badf12133f49dd1a7dfbbaa854de5d63
Emmanuel Babatunde <iambabatundes@gmail.com>
2023-02-09 04:05:26 -0800

    Connecting to database

--- start/db.js
+++ start/db.js
@@ -1,11 +1,22 @@
 const winston = require("winston");
 const mongoose = require("mongoose");
-const config = require("config");
 
 module.exports = function () {
   mongoose.set("strictQuery", true);
 
-  const db = config.get("db");
+  mongoose
+    .connect(process.env.DB_URL)
+    .then(() => winston.info(`Connected to ${process.env.DB_URL}`));
 
-  mongoose.connect(db).then(() => winston.info(`Connected to ${db}...`));
+  // mongoose.connect(db).then(() => winston.info(`Connected to ${db}...`));
 };
+
+// MongoDB
+
+// const MongoClient = require('mongodb').MongoClient;
+
+// MongoClient.connect(process.env.DB_URL, function(err, db) {
+//   if(!err) {
+//     console.log("We are connected");
+//   }
+// });
